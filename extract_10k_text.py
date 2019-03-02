075438cfd78f34db856711e3c499decf1d49bc31
Deepak Kumar <deepakjon31@gmail.com>
2019-03-02 16:49:51 +0530

    updated

--- extract_10k_text.py
+++ extract_10k_text.py
@@ -8,8 +8,6 @@ from bs4 import SoupStrainer as ST
 LXML = 'lxml'
 ONLY_FONT_TAGS = 'div'
 NoneType = type(None)
-DIGITS = [i for i in range(0, 101)]
-print(DIGITS)
 
 
 class ExtractText(object):
@@ -25,20 +23,7 @@ class ExtractText(object):
         except (ConnectionError, ConnectTimeout) as e:
             print(f'Error: {e}')
             traceback.print_exc(e)
-
-    def _filter(self, text):
-        text_len = len(text.strip())
-        if isinstance(text, NoneType):
-            return False, 0
-        elif text_len < 3:
-            return False, text_len
-        elif 'None' in text:
-            return False, 0
-        elif text_len < 21:
-            return True, text_len
-        elif text_len > 20:
-            return True, text_len
-
+            
     def extract_10k_text(self, url):
         soup = BS(self._get_response(url), LXML, parse_only=self.only_font_tags)
         fonts = soup.find_all('font')
@@ -65,38 +50,7 @@ class ExtractText(object):
         data = pd.DataFrame(table)
         data.to_csv('deepak_10k.csv', index=False)
 
-        # divs = soup.find_all('div')
-        # previous_header = ''
-        # temp_text = ''
-        # table = []
-        # for div in divs:
-        #     text = div.getText(strip=' ')
-        #     if text is None or len(text.strip()) == 0: continue
-        #
-        #     filter_text, text_len = self._filter(text)
-        #     if filter_text:
-        #         text = text.strip()
-        #         if text_len < 21:
-        #             print(temp_text)
-        #             table.append((previous_header, temp_text))
-        #             previous_header = text
-        #             temp_text = ''
-        #             print(previous_header, '******************************')
-        #         # elif text_len > 20 and previous_header == text:
-        #         #     temp_text = temp_text + ' ' + text
-        #
-        #         # print(previous_header, '\n', temp_text)
-        #         else:
-        #             print('------------------\n')
-        #             print(len(text.strip()), text)
-        #             temp_text = temp_text + ' ' + text
-        # import pandas as pd
-        # data = pd.DataFrame(table)
-        # data.to_csv('deepak_10k.csv', index=False)
-
-
-
-
+        
 if __name__ == "__main__":
     obj = ExtractText()
     obj.extract_10k_text('https://www.sec.gov/Archives/edgar/data/1018724/000101872419000004/amzn-20181231x10k.htm')
